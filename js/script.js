720445c222c57fd2ce7aa965735b07571184972a
Liza <puzanovaliza6666@mail.ru>
2024-02-15 01:58:44 +0300

    Co-authored-by: Nikita Lvov <Slark58@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,22 @@
+AOS.init();
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// const navToggle = document.querySelector('#nav-toggle');
+// const navList = document.querySelector('#nav-list');
+
+// navToggle.addEventListener('click', () => {
+//   navList.toggleAttribute('hidden');
+// });
+
