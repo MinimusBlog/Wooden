8c2b41388e41569ed96fbd5ba3f4410c088e8050
Liza <puzanovaliza6666@mail.ru>
2024-02-15 19:27:30 +0300

    connect db + comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-AOS.init();
 
-window.onscroll = function() {myFunction()};
+AOS.init(); // Подключение библиотеки анимаций
+
+window.onscroll = function() {myFunction()}; //Обновленеие страницы при нажатии на лого
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
@@ -12,6 +13,33 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+const { Client } = require('pg'); // Подключение к БД
+
+// экземпляр клиента PostgreSQL
+const client = new Client({
+  user: 'User',
+  host: 'localhost',
+  database: 'wooden',
+  password: '12345',
+  port: 5432, // Порт по умолчанию для PostgreSQL
+});
+
+// Подключение к базе данных
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err.stack));
+
+// Пример выполнения SQL-запроса
+client.query('SELECT * FROM your_table')
+  .then(result => console.log(result.rows))
+  .catch(err => console.error('Query error', err.stack));
+
+// Закрытие соединения с базой данных
+client.end()
+  .then(() => console.log('Connection closed'))
+  .catch(err => console.error('Error closing connection', err.stack));
+
 
 // const navToggle = document.querySelector('#nav-toggle');
 // const navList = document.querySelector('#nav-list');
