3be9c3f787209639860497a5b3be9393049b0578
Liza <puzanovaliza6666@mail.ru>
2024-02-17 15:32:54 +0300

    fix structure folder, css etc..

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import "./script";
+import "./gallery";
+import "./switcher-pages";
+// import Swiper JSs
+
+//Library
+// import "./aos"
+
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
+});
+
+
+
