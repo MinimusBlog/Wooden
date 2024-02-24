0c1315be7b65639ebf62adcfb1e2c6539774f20f
Liza <puzanovaliza6666@mail.ru>
2024-02-24 17:45:31 +0300

    functioning authorization and registration

--- js/switcher-pages.js
+++ js/switcher-pages.js
@@ -12,97 +12,97 @@ function handleScroll() {
 window.addEventListener('scroll', handleScroll);
 
 
-//! popup log in, reg..
-document.addEventListener('DOMContentLoaded', function () {
-    // Получение элементов DOM
-    const loginLink = document.getElementById('login__link');
-    const popupOverlay = document.querySelector('.popup__overlay');
-    const popup = document.querySelector('.popup');
-    const registerButton = document.querySelector('.toggle__form');
-    const loginForm = document.getElementById('login__form');
-    const registerForm = document.getElementById('register__form');
+// //! popup log in, reg..
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Получение элементов DOM
+//     const loginLink = document.getElementById('login__link');
+//     const popupOverlay = document.querySelector('.popup__overlay');
+//     const popup = document.querySelector('.popup');
+//     const registerButton = document.querySelector('.toggle__form');
+//     const loginForm = document.getElementById('login__form');
+//     const registerForm = document.getElementById('register__form');
 
-    //* Функция для сброса значений полей формы
-    function resetForm(form) {
-        form.querySelectorAll('input').forEach(input => {
-            input.value = ''; //* Сбрасываем значение поля
-        });
-    }
+//     //* Функция для сброса значений полей формы
+//     function resetForm(form) {
+//         form.querySelectorAll('input').forEach(input => {
+//             input.value = ''; //* Сбрасываем значение поля
+//         });
+//     }
 
-    //* Обработчик события клика по ссылке для открытия попапа
-    loginLink.addEventListener('click', function (event) {
-        event.preventDefault(); 
-        popupOverlay.style.display = 'block'; 
-        popup.classList.remove('hidden'); 
-        resetForm(loginForm); //* Сбрасываем значения полей формы входа
-        resetForm(registerForm); //* Сбрасываем значения полей формы регистрации
-        document.body.classList.add('modal__open'); //* Добавляем класс для блокировки прокрутки страницы
-    });
+//     //* Обработчик события клика по ссылке для открытия попапа
+//     loginLink.addEventListener('click', function (event) {
+//         event.preventDefault(); 
+//         popupOverlay.style.display = 'block'; 
+//         popup.classList.remove('hidden'); 
+//         resetForm(loginForm); //* Сбрасываем значения полей формы входа
+//         resetForm(registerForm); //* Сбрасываем значения полей формы регистрации
+//         document.body.classList.add('modal__open'); //* Добавляем класс для блокировки прокрутки страницы
+//     });
 
-    //* Обработчик события клика по кнопке закрытия попапа
-    const closePopup = document.querySelector('.close__popup');
-    closePopup.addEventListener('click', function () {
-        popupOverlay.style.display = 'none'; //* Скрываем оверлей
-        popup.classList.add('hidden'); //* Добавляем класс скрытия попапа
-        document.body.classList.remove('modal__open'); //* Удаляем класс для разблокировки прокрутки страницы
-    });
+//     //* Обработчик события клика по кнопке закрытия попапа
+//     const closePopup = document.querySelector('.close__popup');
+//     closePopup.addEventListener('click', function () {
+//         popupOverlay.style.display = 'none'; //* Скрываем оверлей
+//         popup.classList.add('hidden'); //* Добавляем класс скрытия попапа
+//         document.body.classList.remove('modal__open'); //* Удаляем класс для разблокировки прокрутки страницы
+//     });
 
-    //* Обработчик события отправки формы входа
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault(); 
-        resetForm(loginForm); //* Сбрасываем значения полей формы входа
-    });
+//     //* Обработчик события отправки формы входа
+//     loginForm.addEventListener('submit', function(event) {
+//         event.preventDefault(); 
+//         resetForm(loginForm); //* Сбрасываем значения полей формы входа
+//     });
 
-    //* Обработчик события отправки формы регистрации
-    registerForm.addEventListener('submit', function(event) {
-        event.preventDefault(); 
-        resetForm(registerForm); //* Сбрасываем значения полей формы регистрации
-    });
+//     //* Обработчик события отправки формы регистрации
+//     registerForm.addEventListener('submit', function(event) {
+//         event.preventDefault(); 
+//         resetForm(registerForm); //* Сбрасываем значения полей формы регистрации
+//     });
 
-    //* Обработчик события клика по кнопке переключения формы
-    registerButton.addEventListener('click', function () {
-        if (loginForm.style.display === 'block') {
-            loginForm.style.display = 'none';
-            registerForm.style.display = 'block';
-            registerButton.textContent = 'Войти';
-        } else {
-            registerForm.style.display = 'none';
-            loginForm.style.display = 'block';
-            registerButton.textContent = 'Зарегистрироваться';
-        }
-    });
-});
+//     //* Обработчик события клика по кнопке переключения формы
+//     registerButton.addEventListener('click', function () {
+//         if (loginForm.style.display === 'block') {
+//             loginForm.style.display = 'none';
+//             registerForm.style.display = 'block';
+//             registerButton.textContent = 'Войти';
+//         } else {
+//             registerForm.style.display = 'none';
+//             loginForm.style.display = 'block';
+//             registerButton.textContent = 'Зарегистрироваться';
+//         }
+//     });
+// });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const registerForm = document.getElementById('register__form');
-    registerForm.addEventListener('submit', async function(event) {
-        event.preventDefault(); // Предотвращаем стандартное поведение формы (перезагрузка страницы)
+// document.addEventListener('DOMContentLoaded', function() {
+//     const registerForm = document.getElementById('register__form');
+//     registerForm.addEventListener('submit', async function(event) {
+//         event.preventDefault(); // Предотвращаем стандартное поведение формы (перезагрузка страницы)
         
-        const username = document.getElementById('register__username').value;
-        const password = document.getElementById('register__password').value;
+//         const username = document.getElementById('register__username').value;
+//         const password = document.getElementById('register__password').value;
 
-        try {
-            const response = await fetch('/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, password })
-            });
+//         try {
+//             const response = await fetch('/register', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify({ username, password })
+//             });
 
-            if (response.ok) {
-                const data = await response.json();
-                console.log('User registered:', data.user);
-                // Дополнительные действия, например, перенаправление на другую страницу
-            } else {
-                console.error('Registration failed');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    });
-});
+//             if (response.ok) {
+//                 const data = await response.json();
+//                 console.log('User registered:', data.user);
+//                 // Дополнительные действия, например, перенаправление на другую страницу
+//             } else {
+//                 console.error('Registration failed');
+//             }
+//         } catch (error) {
+//             console.error('Error:', error);
+//         }
+//     });
+// });
 
 
 //! переключение в нав меню на галерею
---
+++ server.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
+
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/users/register", checkAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/users/login", checkAuthenticated, (req, res) => {
+  // flash sets a messages variable. passport sets the error message
+  console.log(req.session.flash && req.session.flash.error);
+  res.render("login.ejs");
+});
+
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+  console.log(req.isAuthenticated());
+  res.render("dashboard", { user: req.user }); // Передаем весь объект пользователя
+});
+
+
+app.get("/users/logout", (req,res) => {
+    req.logOut(function(err) {
+        if (err) { return next(err); }
+        req.flash("success_msg", "You have successfully logged out.");
+        res.redirect("/users/login");
+    });
+});
+
+app.post("/users/register", async (req, res) => {
+  let { login, password, password2 } = req.body;
+
+  // Определяем someValue
+  const someValue = req.body.someValue; //someValue передается из запроса
+
+  let errors = [];
+
+  console.log({
+    login,
+    password,
+    password2
+  });
+
+  if (!login || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password must be at least 6 characters long" });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors, login, password, password2 });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM wood.users WHERE login = $1`,
+      [login],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          return res.render("register", {
+            message: "Login already registered"
+          });
+        } else {
+          pool.query(
+            `INSERT INTO wood.users (login, password, createdat, updatedat, idtechnicalsupport)
+            VALUES ($1, $2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, $3)
+            RETURNING iduser, login`, // возвращаем iduser и login созданной записи
+            [login, hashedPassword, someValue],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/dashboard");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
