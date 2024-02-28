4ef2dfbe292918a715dc7483b7768140e305b5e9
Liza <puzanovaliza6666@mail.ru>
2024-02-28 13:01:03 +0300

    category

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@
 //   navList.toggleAttribute('hidden');
 // });
 
+
+function openCategory(category) {
+    const categories = document.querySelectorAll('.category');
+    categories.forEach(cat => {
+        cat.classList.remove('selected');
+    });
+
+    const selectedCategory = document.querySelector(`.category[data-category="${category}"]`);
+    selectedCategory.classList.add('selected');
+
+    const products = document.querySelectorAll('.product__card');
+    products.forEach(product => {
+        const productCategories = product.classList;
+        if (category === 'all') {
+            product.style.visibility = 'visible';
+        } else if (category === 'home') {
+            if (productCategories.contains('home')) {
+                product.style.visibility = 'visible';
+            } else {
+                product.style.visibility = 'hidden';
+            }
+        } else if (productCategories.contains(category)) {
+            product.style.visibility = 'visible';
+        } else {
+            product.style.visibility = 'hidden';
+        }
+    });
+}
