be99c2e8ea17c2ef1b3ef0eed0c8bc6e2dfdaa60
Liza <puzanovaliza6666@mail.ru>
2024-03-27 01:05:42 +0300

    Was added new features to bot

--- bot.py
+++ bot.py
@@ -1,7 +1,23 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('6756052562:AAG7R51KH84u_ao4lGSLLuFXDUMHWbmb64Y')
+bot_token = '6756052562:AAG7R51KH84u_ao4lGSLLuFXDUMHWbmb64Y'
+operators = {'123456789': 'Operator 1', '987654321': 'Operator 2'}  # Словарь ID операторов и их имен
+
+bot = telebot.TeleBot(bot_token)
+user_requests = {}  # Словарь для хранения запросов на связь с оператором
+
+orders = {
+    '100667': 'Заказ оформлен',
+    '125668': 'Заказ доставлен',
+    '130668': 'Заказ в пути'
+}
+
+def is_operator(user_id):
+    return user_id in operators
+
+def send_to_user(user_id, text):
+    bot.send_message(user_id, text)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -10,11 +26,35 @@ def start(message):
     markup.row(types.KeyboardButton('Связаться с оператором'), types.KeyboardButton('Перейти на сайт'))
     file = open('./ava.jpg', 'rb')
     bot.send_photo(message.chat.id, file, reply_markup=markup)
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!', reply_markup=markup)
 
 @bot.message_handler(func=lambda message: message.text == 'Связаться с оператором')
 def contact_operator(message):
+    user_requests[message.chat.id] = message.from_user.id  # Сохраняем запрос на связь с оператором
     bot.send_message(message.chat.id, 'Оператор свяжется с вами в ближайшее время.')
+
+@bot.message_handler(commands=['connect_operator'])
+def connect_operator(message):
+    bot.send_message(message.chat.id, 'Введите свой ID:')
+    bot.register_next_step_handler(message, process_operator_id)
+
+def process_operator_id(message):
+    operator_id = message.text.strip()
+    if operator_id in operators:
+        bot.send_message(message.chat.id, 'Теперь вы оператор!')
+        operators[operator_id] = message.from_user.username  # Добавляем оператора в список
+        for chat_id, user_id in user_requests.items():
+            send_to_user(operator_id, f'Пользователь {user_id} хотел уточнить что-то.')
+    else:
+        bot.send_message(message.chat.id, 'Введенный ID оператора не найден.')
+
+def process_operator_id(message):
+    operator_id = message.text.strip()
+    if operator_id in operators:
+        for chat_id, user_id in user_requests.items():
+            send_to_user(operator_id, f'Пользователь {user_id} хотел уточнить что-то.')
+    else:
+        bot.send_message(message.chat.id, 'Введенный ID оператора не найден.')
 
 @bot.message_handler(func=lambda message: message.text == 'Перейти на сайт')
 def contact_operator(message):
@@ -22,15 +62,28 @@ def contact_operator(message):
     markup.add(types.InlineKeyboardButton('Перейти на сайт', url='http://www.google.com'))
     file = open('./ava.jpg', 'rb')
     bot.send_photo(message.chat.id, file, reply_markup=markup, caption='Переход на сайт Wooden!')
-    #bot.send_message(message.chat.id, 'Переход на сайт Wooden!', reply_markup=markup)
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(func=lambda message: message.text == 'Статус заказа')
+def check_order_status(message):
+    bot.send_message(message.chat.id, 'Введите номер заказа:')
+    bot.register_next_step_handler(message, process_order_status)
+
+def process_order_status(message):
+    order_number = message.text
+    if order_number in orders:
+        bot.send_message(message.chat.id, f'Статус заказа {order_number}: {orders[order_number]}')
+    else:
+        bot.send_message(message.chat.id, 'Заказ с таким номером не найден.')
+
+@bot.message_handler(func=lambda message: message.text == 'Помощь')
 def help_message(message):
-    bot.send_message(message.chat.id, '''Добро пожаловать в службу технической поддержки!
+    instructions = """
+    Привет! Это инструкции по использованию бота:
 
-Мы здесь, чтобы помочь вам решить любые технические проблемы или вопросы, с которыми вы столкнулись. Вот некоторые полезные команды, которые вы можете использовать:
-
-/help - отображает это сообщение с информацией о доступных командах.
-/site - отображает наш сайт с товарами.''')
+    1. Связаться с оператором: Нажмите кнопку "Связаться с оператором", чтобы запросить помощь оператора.
+    2. Перейти на сайт: Нажмите кнопку "Перейти на сайт", чтобы перейти на сайт Wooden.
+    3. Статус заказа: Нажмите кнопку "Статус заказа", чтобы проверить статус вашего заказа.
+    """
+    bot.send_message(message.chat.id, instructions)
 
 bot.polling(none_stop=True)
